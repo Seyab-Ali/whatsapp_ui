528814a9909337ccbf585686964672211bbf5c5a
seyab ali <seyabalimkd@gmail.com>
2023-04-09 18:02:32 +0500

    hi

---
+++ lib/directory/directory.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class directory extends StatefulWidget {
+  const directory({Key? key}) : super(key: key);
+
+  @override
+  State<directory> createState() => _directoryState();
+}
+
+class _directoryState extends State<directory> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
